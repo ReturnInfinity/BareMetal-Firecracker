a4972f2e21c91e93922e22852a345a3d28dada0c
Ian Seyler <ian@seyler.me>
2026-05-29 18:30:48 -0400

    Add APIC registers

--- src/init/cpu.asm
+++ src/init/cpu.asm
@@ -103,6 +103,38 @@ IA32_MTRR_PHYSMASK1	equ 0x203
 IA32_PAT		equ 0x277
 IA32_MTRR_DEF_TYPE	equ 0x2FF
 
+; APIC Register list
+; 0x000 - 0x010 are Reserved
+APIC_ID		equ 0x020		; ID Register
+APIC_VER	equ 0x030		; Version Register
+; 0x040 - 0x070 are Reserved
+APIC_TPR	equ 0x080		; Task Priority Register
+APIC_APR	equ 0x090		; Arbitration Priority Register
+APIC_PPR	equ 0x0A0		; Processor Priority Register
+APIC_EOI	equ 0x0B0		; End Of Interrupt
+APIC_RRD	equ 0x0C0		; Remote Read Register
+APIC_LDR	equ 0x0D0		; Logical Destination Register
+APIC_DFR	equ 0x0E0		; Destination Format Register
+APIC_SPURIOUS	equ 0x0F0		; Spurious Interrupt Vector Register
+APIC_ISR	equ 0x100		; In-Service Register (Starting Address)
+APIC_TMR	equ 0x180		; Trigger Mode Register (Starting Address)
+APIC_IRR	equ 0x200		; Interrupt Request Register (Starting Address)
+APIC_ESR	equ 0x280		; Error Status Register
+; 0x290 - 0x2E0 are Reserved
+APIC_ICRL	equ 0x300		; Interrupt Command Register (low 32 bits)
+APIC_ICRH	equ 0x310		; Interrupt Command Register (high 32 bits)
+APIC_LVT_TMR	equ 0x320		; LVT Timer Register
+APIC_LVT_TSR	equ 0x330		; LVT Thermal Sensor Register
+APIC_LVT_PERF	equ 0x340		; LVT Performance Monitoring Counters Register
+APIC_LVT_LINT0	equ 0x350		; LVT LINT0 Register
+APIC_LVT_LINT1	equ 0x360		; LVT LINT1 Register
+APIC_LVT_ERR	equ 0x370		; LVT Error Register
+APIC_TMRINITCNT	equ 0x380		; Initial Count Register (for Timer)
+APIC_TMRCURRCNT	equ 0x390		; Current Count Register (for Timer)
+; 0x3A0 - 0x3D0 are Reserved
+APIC_TMR_DIV	equ 0x3E0		; Divide Configuration Register (for Timer)
+; 0x3F0 is Reserved
+
 
 ; =============================================================================
 ; EOF
