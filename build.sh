4c3de44e61facebad889b32c7dd801aa23326dff
Ian Seyler <ian.seyler@returninfinity.com>
2026-06-14 20:55:11 -0400

    Test for padding kernel out with app at end to align at 0x200000

--- build.sh
+++ build.sh
@@ -21,7 +21,8 @@ cd ..
 cd ..
 
 cd sys
-cat kernel.sys monitor.bin > test.sys
+dd if=/dev/zero of=blank.sys count=1030144 bs=1 > /dev/null
+cat kernel.sys monitor.bin blank.sys hello.app > test.sys
 
 objcopy --input-target binary --output-target elf64-x86-64 --binary-architecture i386:x86-64 --rename-section .data=.kernel test.sys kernel_sys.o
 ld -m elf_x86_64 -nostdlib -z max-page-size=0x1000 -T ../src/baremetal.ld -o baremetal.elf init.o kernel_sys.o
