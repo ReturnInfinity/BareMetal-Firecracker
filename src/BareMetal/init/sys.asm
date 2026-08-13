7bc4a29b7e93f40b928de850d3bfe585b1b1a517
Ian Seyler <ian.seyler@returninfinity.com>
2026-08-13 18:23:39 -0400

    Remove debug code

--- src/BareMetal/init/sys.asm
+++ src/BareMetal/init/sys.asm
@@ -16,20 +16,12 @@ init_sys:
 	div ecx
 	mov [os_boot_time], rax		; Store the boot time in os_boot_time
 
-; Debug
 	; Calibrate the APIC timer against KVM
 	call os_apic_timer_calibrate
 
-; Debug
-; TODO - Set as system call to enable
 	; Start the periodic timer
 	mov rax, 1000000		; 1,000,000 ns = 1ms (1000Hz)
 	mov ecx, 0x68
-	call b_system;call os_apic_timer_set
-
-; Debug - test callback
-	mov rax, sys_debug_callback
-	mov ecx, 0x60
 	call b_system
 
 %ifdef DEBUG
@@ -41,19 +33,6 @@ init_sys:
 	ret
 ; -----------------------------------------------------------------------------
 
-sys_debug_callback:
-	push rax
-
-; Debug - Display 01 every X ticks
-	mov rax, [os_ticks]
-	test rax, 0x3FF			; Only print about once a second (every 1024 ticks @ 1000Hz)
-	jnz int_apic_timer_debug_skip
-	mov al, 01
-	call os_debug_dump_al
-int_apic_timer_debug_skip:
-
-	pop rax
-	ret
 
 ; =============================================================================
 ; EOF
